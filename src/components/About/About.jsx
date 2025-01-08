80f2d4c173c17d0c73185d5286f39a977db2f15d
shreya <berashreya05@gmail.com>
2025-01-09 00:08:36 +0530

    minor change

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,11 +11,10 @@ const About = () => {
         
      <div className="img"> <img className='aIcon' src="https://tse1.mm.bing.net/th?id=OIP.fCGTbQP_aEOdSGiaAfbE4AHaHa&pid=Api&P=0&h=220" alt="" /></div>
      <div className="aPara">
-     <h1>About</h1>
-        <br />
-        <br />
       <p>
-      
+        <h1>About</h1>
+        <br />
+        <br />
       Welcome to my portfolio! I’m Shreya Bera, a first-year BTech student pursuing Computer Science and Engineering. My passion for technology drives me to constantly explore new areas of development, and right now, I’m focusing on front-end web development. I am actively learning React, JavaScript, HTML, and CSS to create seamless, user-friendly web experiences. Along with these, I have a solid understanding of C and basic knowledge of C++, and I’m always sharpening my problem-solving skills through coding challenges and projects. I'm driven by curiosity and the desire to build impactful, efficient applications that solve real-world problems. As I continue to grow my skill set, I’m excited to experiment with new technologies and push the boundaries of what’s possible in web development. Thank you for visiting my site—feel free to explore my projects and get in touch!
      
 </p>

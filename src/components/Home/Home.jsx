c5ae04daea7d9e1c9b1db87d94b768ef63c33312
shreya <berashreya05@gmail.com>
2025-01-10 00:16:01 +0530

    aise hi

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,6 +15,7 @@ const Home = () => {
             <span className='hello'>hello,</span>
             <span className='im'>I'm <span className='name'> 
             Shreya Bera</span> <br />Frontend Developer</span>
+            <br />
             <p className='para'>I am a passionate web dev and problem solver.Currently fresher in BTech CSE. <br /> I love to build interactive,creative and user-friendly websites.</p>
             <button className='btn' onClick={scroll}>more about me</button>
             <div className="cls"><a href="https://www.linkedin.com/in/shreya-bera-978878307?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">  <img className='socialIcon' src="https://tse2.mm.bing.net/th?id=OIP.w_zDkEJ9aLiWR-g0rff8hwHaHa&pid=Api&P=0&h=220" alt="" /></a>
